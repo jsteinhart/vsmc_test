6e0edf1507f3e8a8884d3fb6c9dbdfeb016fc262
Jonathan Steinhart <Jonathan.Steinhart@ait.ac.at>
2015-03-02 16:24:57 +0100

    build project with HDF5 support, try to open file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
+#include <vsmc/core/sampler.hpp>
+
+#include "hdf5.h"
+
+#include "H5Cpp.h"
+
 using namespace std;
+using namespace H5;
+
+
+const string DATA_DIR = getenv("VSMC_TEST_DATA_DIR");
+const H5std_string DATA_FILE( DATA_DIR + "/" + "data.h5" );
+
+
+
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    H5File* file = new H5File(DATA_FILE, H5F_ACC_RDONLY);
+    //Group* group = new Group(file->openGroup("/basic"));
+    DataSet* dataset = new DataSet(file->openDataSet("/basic/data"));
     return 0;
 }
