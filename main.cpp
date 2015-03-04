e762895ff3594678d72da2d67a710ccc68596057
Jonathan Steinhart <Jonathan.Steinhart@ait.ac.at>
2015-03-04 11:02:12 +0100

    Read in HDF5 data

--- main.cpp
+++ main.cpp
@@ -10,10 +10,17 @@ using namespace std;
 using namespace H5;
 
 
-const string DATA_DIR = getenv("VSMC_TEST_DATA_DIR");
-const H5std_string DATA_FILE( DATA_DIR + "/" + "data.h5" );
+const H5std_string DATA_FILE( "data/data.h5" );
 
+const H5std_string TIME("time");
+const H5std_string STATE("state");
+const H5std_string OBSERVATION("observation");
 
+struct BasicDatum {
+    int time;
+    int state;
+    double observation;
+};
 
 
 int main() {
@@ -21,5 +28,19 @@ int main() {
     H5File* file = new H5File(DATA_FILE, H5F_ACC_RDONLY);
     //Group* group = new Group(file->openGroup("/basic"));
     DataSet* dataset = new DataSet(file->openDataSet("/basic/data"));
+    DataSpace space = dataset->getSpace();
+    int rank = space.getSimpleExtentNdims(); //should be 1 for now!
+    hsize_t dims[rank];
+    space.getSimpleExtentDims(dims);
+    int N = dims[0];
+    cout << "will read in " << N << " lines of data." << endl;
+    CompType datumType(sizeof(BasicDatum));
+    datumType.insertMember(TIME, HOFFSET(BasicDatum, time), PredType::NATIVE_INT);
+    datumType.insertMember(STATE, HOFFSET(BasicDatum, state), PredType::NATIVE_INT);
+    datumType.insertMember(OBSERVATION, HOFFSET(BasicDatum, observation), PredType::NATIVE_DOUBLE);
+    BasicDatum basicData[N];
+    dataset->read(basicData, datumType);
+    delete dataset;
+    delete file;
     return 0;
 }
